bc9ed9d89aa0b3ef74d9afbb27937812719259a7
Eric Fritz <eric@eric-fritz.com>
2020-04-07 12:13:21 -0500

    Use ssh over https for git operations.

--- .buildkite/upstream.sh
+++ .buildkite/upstream.sh
@@ -5,9 +5,9 @@ set -ex
 OWNER="${1:?missing owner}"
 NAME="${2:?missing name}"
 
-git clone https://github.com/sourcegraph-codeintel-showcase/$NAME.git
+git clone "git@github.com:sourcegraph-codeintel-showcase/$NAME.git"
 pushd "$NAME"
-git remote add upstream https://github.com/$OWNER/$NAME.git
+git remote add upstream git@github.com:$OWNER/$NAME.git
 git fetch upstream master
 git rebase upstream/master master
 GIT_SSH_COMMAND='ssh -i /buildkite/.ssh/codeintel_showcase_upstreamer -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no' git push origin master --force
