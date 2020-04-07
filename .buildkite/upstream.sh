1a3524ab8c23ff5df7a874b82ac80c9a077723df
Eric Fritz <eric@eric-fritz.com>
2020-04-07 12:18:34 -0500

    Add fake git identity.

--- .buildkite/upstream.sh
+++ .buildkite/upstream.sh
@@ -8,6 +8,8 @@ NAME="${2:?missing name}"
 git clone "git@github.com:sourcegraph-codeintel-showcase/$NAME.git"
 pushd "$NAME"
 git remote add upstream git@github.com:$OWNER/$NAME.git
+git config user.name "Codeintel showcase upstreamer"
+git config user.email "upstreamer@sourcegraph.com"
 git fetch upstream master
 git rebase upstream/master master
 GIT_SSH_COMMAND='ssh -i /buildkite/.ssh/codeintel_showcase_upstreamer -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no' git push origin master --force
