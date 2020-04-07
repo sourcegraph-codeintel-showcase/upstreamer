aeaebb72d48cae7011b494830ad6b9d4875f7057
Eric Fritz <eric@eric-fritz.com>
2020-04-07 12:21:08 -0500

    Document upstream script.

--- .buildkite/upstream.sh
+++ .buildkite/upstream.sh
@@ -2,15 +2,26 @@
 
 set -ex
 
-OWNER="${1:?missing owner}"
-NAME="${2:?missing name}"
+OWNER="${1:?repository owner not supplied}"
+NAME="${2:?repository name not supplied}"
 
 git clone "git@github.com:sourcegraph-codeintel-showcase/$NAME.git"
 pushd "$NAME"
+
+# Fetch upstream changes
 git remote add upstream git@github.com:$OWNER/$NAME.git
-git config user.name "Codeintel showcase upstreamer"
-git config user.email "upstreamer@sourcegraph.com"
 git fetch upstream master
+
+# Set git identity for following rebase command
+git config user.name "Codeintel showcase upstreamer"
+git config user.email "codeintel-showcase-upstreamer@sourcegraph.com"
+
+# Re-apply LSIF upload commit
 git rebase upstream/master master
-GIT_SSH_COMMAND='ssh -i /buildkite/.ssh/codeintel_showcase_upstreamer -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no' git push origin master --force
+
+# Push n+1 commits to the showcase repository
+# This requires a special private key on the buildkite agents
+GIT_SSH_COMMAND='ssh -i /buildkite/.ssh/codeintel_showcase_upstreamer -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no' \
+    git push origin master --force
+
 popd
